0cb3eec9fdc44117d803a299fb59b2b5f2fd95fc
David Pritchard <david.al.pritchard@gmail.com>
2022-11-20 22:37:17 -0500

    add Vigenere cipher

--- ch14-validating-ciphers/src/Ciphers.hs
+++ ch14-validating-ciphers/src/Ciphers.hs
@@ -5,10 +5,26 @@ import Data.Char ( chr, isLower, isUpper, ord )
 caesar :: Int -> String -> String
 caesar shiftBy = map (caesarLetter shiftBy)
 
+vigenere :: String -> String -> String
+vigenere [] message      = message
+vigenere keyword message = go offsets message
+  where
+
+    offsets = map calcOffset keyword
+      where
+        calcOffset c
+          | isUpper c = ord c - ord 'A'
+          | isLower c = ord c - ord 'a'
+          | otherwise = 0
+
+    go _ []          = []
+    go [] ms         = go offsets ms
+    go (o:os) (m:ms) = caesarLetter o m : go os ms
+
 caesarLetter :: Int -> Char -> Char
 caesarLetter shiftBy c
   | isUpper c = shiftLetter shiftBy (ord 'A') c
-  | isLower c = shiftLetter shiftBy (ord 'A') c
+  | isLower c = shiftLetter shiftBy (ord 'a') c
   | otherwise = c
 
 shiftLetter :: Int -> Int -> Char -> Char
