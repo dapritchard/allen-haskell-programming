fff6c6c59aafe6327e8558880fbf957004e2d586
David Pritchard <david.al.pritchard@gmail.com>
2022-11-20 20:32:51 -0500

    add caesar encoding first pass

--- ch14-validating-ciphers/src/Ciphers.hs
+++ ch14-validating-ciphers/src/Ciphers.hs
@@ -1,1 +1,23 @@
 module Ciphers where
+
+import Data.Char ( chr, isLower, isUpper, ord )
+
+caesar :: Int -> String -> String
+caesar shiftBy = map (caesarLetter shiftBy)
+
+caesarLetter :: Int -> Char -> Char
+caesarLetter shiftBy c
+  | isUpper c = shiftLetter shiftBy (ord 'A') c
+  | isLower c = shiftLetter shiftBy (ord 'A') c
+  | otherwise = c
+
+shiftLetter :: Int -> Int -> Char -> Char
+shiftLetter shiftBy offset letter =
+  let normLetter = ord letter - offset
+      shiftedNormLetter = shiftNormLetter shiftBy normLetter
+  in  chr $ shiftedNormLetter + offset
+
+shiftNormLetter :: Int -> Int -> Int
+shiftNormLetter shiftBy normLetter =
+  let shifted = (shiftBy + normLetter) `rem` 26
+  in  if shifted >= 0 then shifted else shifted + 26
