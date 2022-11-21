cc44c1dbd14730b07de6065e7505a2afeb5a13e8
David Pritchard <david.al.pritchard@gmail.com>
2022-11-20 23:02:42 -0500

    begin writing tests

--- ch14-validating-ciphers/test/CiphersTest.hs
+++ ch14-validating-ciphers/test/CiphersTest.hs
@@ -1,4 +1,33 @@
 module CiphersTest where
 
+import Ciphers ( caesar, vigenere )
+import Test.Hspec ( describe, hspec, it, shouldBe )
+import Test.QuickCheck ( Testable(..) )
+
 main :: IO ()
-main = print ""
+main = hspec $ do
+
+  describe "test caesar" $ do
+
+    describe "empty string" $ do
+      it "shift 2" $ do
+        caesar 2 ""`shouldBe` ""
+
+    describe "no shift" $ do
+      it "shift 0" $ do
+        caesar 0 fox`shouldBe` fox
+      it "shift 52" $ do
+        caesar 52 fox`shouldBe` fox
+      it "shift -52" $ do
+        caesar (-52) fox`shouldBe` fox
+
+    describe "some shift" $ do
+      it "shift 2" $ do
+        caesar 2 fox `shouldBe` "VJG swkem dtqyp hqz lworgf qxgt vjg ncba fqi"
+      it "shift -2" $ do
+        caesar (-2) fox
+          `shouldBe`
+          "RFC osgai zpmul dmv hskncb mtcp rfc jyxw bme"
+
+fox :: String
+fox = "THE quick brown fox jumped over the lazy dog"
